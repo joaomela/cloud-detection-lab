507684524513ba03b85bc1f0cac8cf72587c5a40
Joao Mela <joao.mela.t0132174@edu.atec.pt>
2026-01-28 16:05:44 +0000

    error 1.0

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "192.168.1.0/24"
+  cidr_block              = "192.168.0.0/24"
   map_public_ip_on_launch = true
 
   tags = {
